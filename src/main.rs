9d823f6072f91ab0adf5882693f8909ed65e82a1
amirjahan1 <amirjahan454@gmail.com>
2024-11-08 17:56:26 +0330

    Struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+#![allow(dead_code)]
+#[derive(Debug)]
+struct Person {
+    name: String,
+    age: u8,
+}
+ 
+
+// A struct with two fields 
+
 fn main() {
-    println!("Hello, world!");
+    // Create struct with field init shorthand
+    let name = String::from("Peter");
+    let age = 27;
+    let peter = Person { name, age };
+
+    // Print debug struct
+    println!("{:?}", peter);
+
+     
+ 
 }
